9d59c26f3150ac04afab906484a672cc69fb63f4
Anar Seferov <1anarseferov@gmail.com>
2020-12-07 18:13:51 +0400

    Updates

---
+++ precache-manifest.a6551019d47a55214576b59cbe727778.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "709ff306a7290b4186bf100af23d3bc8",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "eb56b5bb8e31d9635dc4",
+    "url": "/movie-app/static/css/main.0f2b6d12.chunk.css"
+  },
+  {
+    "revision": "df622660761c9295aad1",
+    "url": "/movie-app/static/js/2.a65ca716.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app/static/js/2.a65ca716.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb56b5bb8e31d9635dc4",
+    "url": "/movie-app/static/js/main.cf1a8d85.chunk.js"
+  },
+  {
+    "revision": "8c0fe60c15a349e095c4",
+    "url": "/movie-app/static/js/runtime-main.1fbae34b.js"
+  }
+]);
